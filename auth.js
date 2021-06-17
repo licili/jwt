33e9c208a871d65a80ff35fab54c60a39710f951
lici <1035339896@qq.com>
2021-06-17 10:31:10 +0800

    add token 原理

--- auth.js
+++ auth.js
@@ -6,11 +6,9 @@ module.exports = (req, res, next) => {
     try {
       // 过期时间它会自己判断
       let decoded = jwt.decode(authorization.split(' ')[1], secret);
-      console.log(decoded)
       req.user = decoded.user;
       next();
     } catch (err) {
-      console.log(err)
       res.status(401).send('Not Allowed'); // 无权访问
     }
   } else {
